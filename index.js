8c0ab010a849e58c49763f3c1491d8f5f22ae6ce
musabbihanoor <musabbihanoor33@gmail.com>
2023-01-29 21:49:32 +0500

    removed cors origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://kingandknight.netlify.app/",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });

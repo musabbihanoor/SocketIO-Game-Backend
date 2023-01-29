aff84a33ee198a3fc94c98434431c825a11a5901
musabbihanoor <musabbihanoor33@gmail.com>
2023-01-29 20:36:43 +0500

    fixing cors error

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 const cors = require("cors");
 
 app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
+const io = require("socket.io")(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
 });
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
 const rooms = {};
 const roles = ["king", "knight", "theif", "innocent"];

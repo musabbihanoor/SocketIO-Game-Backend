17d6eef817bcf808a3c04372daa1e722de776fbe
musabbihanoor <musabbihanoor33@gmail.com>
2023-01-29 20:43:10 +0500

    fixing cors error

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
+    origin: "https://scary-transport.surge.sh/",
     methods: ["GET", "POST"],
   },
 });
